ae8b46845a04c3f33cdc725fbffb2ecf961536b7
David Nunez <arizonatribe@gmail.com>
2015-12-06 10:22:51 -0700

    Updated with latest redis changes from upstream repo

--- vagrant/pg_config.sh
+++ vagrant/pg_config.sh
@@ -6,6 +6,10 @@ pip install bleach
 pip install oauth2client
 pip install requests
 pip install httplib2
+pip install redis
+pip install passlib
+pip install itsdangerous
+pip install flask-httpauth
 su postgres -c 'createuser -dRS vagrant'
 su vagrant -c 'createdb'
 su vagrant -c 'createdb forum'
@@ -14,3 +18,8 @@ su vagrant -c 'psql forum -f /vagrant/forum/forum.sql'
 vagrantTip="[35m[1mThe shared directory is located at /vagrant\nTo access your shared files: cd /vagrant(B[m"
 echo -e $vagrantTip > /etc/motd
 
+wget http://download.redis.io/redis-stable.tar.gz
+tar xvzf redis-stable.tar.gz
+cd redis-stable
+make
+make install
